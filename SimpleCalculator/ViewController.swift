e604319b374f318b38e56a5611ee4f63eb188156
Wen Li <wenlilearn@gmail.com>
2022-10-16 16:21:53 -0400

    First

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -8,12 +8,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var firstText: UITextField!
+  @IBOutlet weak var secondText: UITextField!
+  @IBOutlet weak var resultLabel: UILabel!
+  var myNumber = 1
+  
+  
+  @IBAction func sumClicked(_ sender: Any) {
+    print(myNumber)
+    // first ! makes sure firstText.text is here
+    // second ! makes sure Int() definitely converts to Int
+    if let firstNumber = Int(firstText.text!) {
+      if let secondNumber = Int(secondText.text!) {
+        let result = firstNumber + secondNumber
+        resultLabel.text = String(result)
+      }
+    }
 
+  }
+  
+  @IBAction func minusClicked(_ sender: Any) {
+    if let firstNumber = Int(firstText.text!) {
+      if let secondNumber = Int(secondText.text!) {
+        let result = firstNumber - secondNumber
+        resultLabel.text = String(result)
+      }
+    }
+  }
+  
+  @IBAction func multiplyClicked(_ sender: Any) {
+    if let firstNumber = Int(firstText.text!) {
+      if let secondNumber = Int(secondText.text!) {
+        let result = firstNumber * secondNumber
+        resultLabel.text = String(result)
+      }
+    }
+  }
+  
+  @IBAction func divideClicked(_ sender: Any) {
+    if let firstNumber = Int(firstText.text!) {
+      if let secondNumber = Int(secondText.text!) {
+        let result = firstNumber / secondNumber
+        resultLabel.text = String(result)
+      }
+    }
+  }
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    myNumber = 2
   }
 
+  
 
 }
 
